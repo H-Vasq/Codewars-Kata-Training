e84e88748587f1ecc09e3728cff9efec6388f748
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-10-15 12:26:53 -0600

    Update Oct--15_to_20.js

--- 8kyu/Oct--15_to_20.js
+++ 8kyu/Oct--15_to_20.js
@@ -1,1 +1,14 @@
+// Create a function with two arguments that will return an array of the first n multiples of x.
 
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+/*COUNT BY X*/
+function countBy(x, n) {
+  let result = [];
+  for (let i = 1; i <= n; i++) {
+     result.push(x * i);
+  }
+  return result;
+}
