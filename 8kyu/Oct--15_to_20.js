eb671721ee7d615d78045e1c492c9318096c9675
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-10-15 12:31:24 -0600

    Update Oct--15_to_20.js

--- 8kyu/Oct--15_to_20.js
+++ 8kyu/Oct--15_to_20.js
@@ -12,3 +12,12 @@ function countBy(x, n) {
   }
   return result;
 }
+
+/*-----------*/
+
+/*Reversed Strings*/
+// Complete the solution so that it reverses the string passed into it.
+
+function solution(str){
+  return str.split('').reverse().join('');
+}
